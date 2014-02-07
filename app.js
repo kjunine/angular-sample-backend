b574adbb5eb9923d1f15c79f5509561cc313c6fc
Daniel Ku <kjunine@gmail.com>
2014-02-07 17:36:20 +0900

    Yeoman으로 생성된 코드 정리

---
+++ app.js
@@ -0,0 +1,3 @@
+app = require('./app/index.js');
+
+module.exports = app.resolve();
---
+++ app/lib/routes.js
@@ -0,0 +1,5 @@
+module.exports = function (app, middleware, HomeController) {
+
+  //app.get('/', HomeController.index);
+
+};
